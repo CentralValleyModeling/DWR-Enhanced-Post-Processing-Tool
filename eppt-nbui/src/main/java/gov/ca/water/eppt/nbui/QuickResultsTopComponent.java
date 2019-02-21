1464d4e79dc2c2181c615cf7faaf4e5fcf703247
Adam Korynta <adam@rmanet.com>
2019-02-21 14:10:50 -0800

    DE-22 #comment remove unused menu and tool bar actions in Netbeans Platform

---
+++ eppt-nbui/src/main/java/gov/ca/water/eppt/nbui/QuickResultsTopComponent.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2019
+ * California Department of Water Resources
+ * All Rights Reserved.  DWR PROPRIETARY/CONFIDENTIAL.
+ * Source may not be released without written approval from DWR
+ */
+package gov.ca.water.eppt.nbui;
+
+import org.openide.awt.ActionID;
+import org.openide.awt.ActionReference;
+import org.openide.util.NbBundle.Messages;
+import org.openide.windows.TopComponent;
+
+/**
+ * Top component which displays something.
+ */
+@TopComponent.Description(
+		preferredID = "QuickResultsTopComponent",
+		//iconBase="SET/PATH/TO/ICON/HERE",
+		persistenceType = TopComponent.PERSISTENCE_ALWAYS
+)
+@TopComponent.Registration(mode = "editor", openAtStartup = true)
+@ActionID(category = "Window", id = "gov.ca.water.eppt.nbui.QuickResultsTopComponent")
+@ActionReference(path = "Menu/Window" /*
+ * , position = 333
+ */)
+@TopComponent.OpenActionRegistration(
+		displayName = "#CTL_QuickResultsAction",
+		preferredID = "QuickResultsTopComponent"
+)
+@Messages(
+		{
+				"CTL_QuickResultsAction=QuickResults",
+				"CTL_QuickResultsTopComponent=QuickResults Window",
+				"HINT_QuickResultsTopComponent=This is a QuickResults window"
+		})
+public final class QuickResultsTopComponent extends TopComponent
+{
+
+	public QuickResultsTopComponent()
+	{
+		setName("Quick Results");
+		setToolTipText(null);
+	}
+
+}
