06937084d91cb9a3f28284da8edaf358402a0b68
Adam Korynta <adam@rmanet.com>
2019-02-22 08:03:35 -0800

    DE-35 #comment last piece was to bundle the eppt.conf file in the maven build. found directions at http://www.mojohaus.org/nbm-maven-plugin/cluster-app-mojo.html from emailing the Netbeans user group users@netbeans.incubator.apache.org

---
+++ application/src/installer/ext/engine/src/org/mycompany/installer/wizard/components/actions/InitializeAction.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright (c) 2019
+ * California Department of Water Resources
+ * All Rights Reserved.  DWR PROPRIETARY/CONFIDENTIAL.
+ * Source may not be released without written approval from DWR
+ */
+package org.mycompany.installer.wizard.components.actions;
+
+import org.netbeans.installer.product.Registry;
+import org.netbeans.installer.utils.StringUtils;
+import org.netbeans.installer.utils.helper.ErrorLevel;
+import org.netbeans.installer.utils.ErrorManager;
+import org.netbeans.installer.utils.ResourceUtils;
+import org.netbeans.installer.utils.progress.Progress;
+import org.netbeans.installer.wizard.components.WizardAction;
+import org.netbeans.installer.wizard.components.actions.*;
+
+public class InitializeAction extends WizardAction {
+    /////////////////////////////////////////////////////////////////////////////////
+    // Instance
+    public InitializeAction() {
+        setProperty(TITLE_PROPERTY,
+                DEFAULT_TITLE);
+        setProperty(DESCRIPTION_PROPERTY,
+                DEFAULT_DESCRIPTION);
+
+        downloadLogic = new DownloadConfigurationLogicAction();
+        initReg = new InitializeRegistryAction();
+    }
+    private DownloadConfigurationLogicAction downloadLogic;
+    private InitializeRegistryAction initReg;
+    
+    public void execute() {
+        final Progress progress = new Progress();
+        
+        //getWizardUi().setProgress(progress);
+        
+
+        progress.setTitle(getProperty(PROGRESS_TITLE_PROPERTY));
+
+        //progress.synchronizeDetails(false);
+
+        if (initReg.canExecuteForward()) {
+            initReg.setWizard(getWizard());
+            initReg.execute();
+        }
+    
+        if (downloadLogic.canExecuteForward()) {
+            downloadLogic.setWizard(getWizard());
+            downloadLogic.execute();
+        }
+    }
+    
+    @Override
+    public boolean isCancelable() {
+        return false;
+    }
+
+    public WizardActionUi getWizardUi() {
+        return null; // this action does not have a ui
+    }
+    
+    /////////////////////////////////////////////////////////////////////////////////
+    // Constants
+    public static final String DEFAULT_TITLE = ResourceUtils.getString(
+            InitializeAction.class, 
+            "IA.title"); // NOI18N
+    public static final String PROGRESS_TITLE_PROPERTY = ResourceUtils.getString(
+            InitializeAction.class, 
+            "IA.progress.title"); // NOI18N
+    public static final String DEFAULT_DESCRIPTION = ResourceUtils.getString(
+            InitializeAction.class, 
+            "IA.description"); // NOI18N
+    
+}
