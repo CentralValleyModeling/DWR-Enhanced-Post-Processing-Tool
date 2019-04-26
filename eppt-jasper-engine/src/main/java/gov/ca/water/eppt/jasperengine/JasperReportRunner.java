e210c74fb2083217874d2412a9a283ce861a1beb
Adam Korynta <adam@rmanet.com>
2019-04-26 10:12:38 -0700

    DE-148 added jasper report runner and test classes for generating report PDF with defaulted xml data

--- eppt-jasper-engine/src/main/java/gov/ca/water/eppt/jasperengine/JasperReportRunner.java
+++ eppt-jasper-engine/src/main/java/gov/ca/water/eppt/jasperengine/JasperReportRunner.java
@@ -12,6 +12,34 @@
 
 package gov.ca.water.eppt.jasperengine;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.stream.Stream;
+
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperCompileManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.SimpleJasperReportsContext;
+import net.sf.jasperreports.engine.export.JRPdfExporter;
+import net.sf.jasperreports.export.SimpleExporterInput;
+import net.sf.jasperreports.export.SimpleOutputStreamExporterOutput;
+import net.sf.jasperreports.repo.FileRepositoryPersistenceServiceFactory;
+import net.sf.jasperreports.repo.FileRepositoryService;
+import net.sf.jasperreports.repo.PersistenceServiceFactory;
+import net.sf.jasperreports.repo.RepositoryService;
+
+import static java.util.stream.Collectors.toList;
+
 /**
  * Company: Resource Management Associates
  *
@@ -20,5 +48,65 @@ package gov.ca.water.eppt.jasperengine;
  */
 public class JasperReportRunner
 {
+	private static final Logger LOGGER = Logger.getLogger(JasperReportRunner.class.getName());
 
+
+	public void runReportWithOutputFile(Path pdfOutputPath, Path jrxmlPath)
+			throws JRException, IOException
+	{
+		Path subreportDir = jrxmlPath.getParent().resolve("Subreports");
+		if(subreportDir.toFile().isDirectory())
+		{
+			try(Stream<Path> paths = Files.walk(subreportDir, 1))
+			{
+				List<Path> subreports = paths.collect(toList());
+				for(Path subreport : subreports)
+				{
+					if(subreport.toFile().isFile() && subreport.toString().endsWith("jrxml"))
+					{
+						LOGGER.log(Level.FINE, "Compiling Subreport {0}", subreport);
+						JasperCompileManager.compileReportToFile(subreport.toString());
+					}
+				}
+			}
+		}
+
+		// compiles jrxml
+		Path dataDir = jrxmlPath.getParent().resolve("Datasource");
+		Path imagesDir = jrxmlPath.getParent().resolve("Images");
+		SimpleJasperReportsContext context = new SimpleJasperReportsContext();
+		FileRepositoryService imageRepository = new FileRepositoryService(context,
+				imagesDir.toString(), true);
+		LOGGER.log(Level.FINE, "Jasper Images Repository {0}", imagesDir);
+		FileRepositoryService fileRepository = new FileRepositoryService(context, jrxmlPath.getParent().toString(),
+				true);
+		LOGGER.log(Level.FINE, "Jasper Subreport Repository {0}", subreportDir);
+		FileRepositoryService subreportRepository = new FileRepositoryService(context,
+				subreportDir.toString(), true);
+		LOGGER.log(Level.FINE, "Jasper Data Repository {0}", dataDir);
+		FileRepositoryService dataRepository = new FileRepositoryService(context,
+				dataDir.toString(), true);
+
+		context.setExtensions(RepositoryService.class,
+				Arrays.asList(fileRepository, imageRepository, subreportRepository, dataRepository));
+		context.setExtensions(PersistenceServiceFactory.class,
+				Collections.singletonList(FileRepositoryPersistenceServiceFactory.getInstance()));
+
+		JasperFillManager manager = JasperFillManager.getInstance(context);
+
+		Map<String, Object> parameters = new HashMap<>();
+		JasperCompileManager.compileReportToFile(jrxmlPath.toString());
+		LOGGER.log(Level.INFO, "Compiling main Jasper Report {0}", jrxmlPath);
+		JasperReport jasperReport = JasperCompileManager.compileReport(jrxmlPath.toString());
+		LOGGER.log(Level.INFO, "Filling Jasper Report with EPPT XML");
+		JasperPrint jasperPrint = manager.fill(jasperReport, parameters);
+
+		LOGGER.log(Level.INFO, "Exporting report to PDF: {0}", pdfOutputPath);
+		// fills compiled report with parameters and a connection
+		JRPdfExporter exporter = new JRPdfExporter();
+		exporter.setExporterInput(new SimpleExporterInput(jasperPrint));
+		exporter.setExporterOutput(new SimpleOutputStreamExporterOutput(pdfOutputPath.toFile()));
+		exporter.exportReport();
+		LOGGER.log(Level.FINE, "Jasper PDF exported");
+	}
 }
---
+++ eppt-jasper-engine/src/test/java/gov/ca/water/eppt/jasperengine/TestJasperReportRunner.java
@@ -0,0 +1,60 @@
+/*
+ * Enhanced Post Processing Tool (EPPT) Copyright (c) 2019.
+ *
+ * EPPT is copyrighted by the State of California, Department of Water Resources. It is licensed
+ * under the GNU General Public License, version 2. This means it can be
+ * copied, distributed, and modified freely, but you may not restrict others
+ * in their ability to copy, distribute, and modify it. See the license below
+ * for more details.
+ *
+ * GNU General Public License
+ */
+
+package gov.ca.water.eppt.jasperengine;
+
+import java.awt.Desktop;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * Company: Resource Management Associates
+ *
+ * @author <a href="mailto:adam@rmanet.com">Adam Korynta</a>
+ * @since 04-26-2019
+ */
+public class TestJasperReportRunner
+{
+	private static final Logger LOGGER = Logger.getLogger(TestJasperReportRunner.class.getName());
+
+	@Test
+	public void testReportRunner() throws Exception
+	{
+		JasperReportRunner jasperReportRunner = new JasperReportRunner();
+		URL resource = getClass().getClassLoader().getResource("QAQC_Report.jrxml");
+		assertNotNull(resource, "JRXML File must be defined");
+		Path jrxmlPath = new File(resource.getPath()).toPath();
+		Path output = Paths.get("Output.pdf");
+		try
+		{
+			Files.delete(output);
+		}
+		catch(NoSuchFileException ex)
+		{
+			LOGGER.log(Level.FINE, "File already deleted", ex);
+		}
+		jasperReportRunner.runReportWithOutputFile(output, jrxmlPath);
+		assertTrue(output.toFile().exists(), "Output PDF should be generated");
+	}
+}
