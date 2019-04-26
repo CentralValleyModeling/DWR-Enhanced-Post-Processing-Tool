e531e99c6da503ce7b246f734c80f33985f3fbd4
Adam Korynta <adam@rmanet.com>
2019-04-26 09:17:49 -0700

    DE-148 added jasper dependencies to pom

---
+++ eppt-jasper-engine/src/main/java/gov/ca/water/eppt/jasperengine/JasperReportRunner.java
@@ -0,0 +1,24 @@
+/*
+ * Enhanced Post Processing Tool (EPPT) Copyright (c) 2019.
+ *
+ * EPPT is copyrighted by the State of California, Department of Water Resources. It is licensed
+ * under the GNU General Public License, version 2. This means it can be
+ * copied, distributed, and modified freely, but you may not restrict others
+ * in their ability to copy, distribute, and modify it. See the license below
+ * for more details.
+ *
+ * GNU General Public License
+ */
+
+package gov.ca.water.eppt.jasperengine;
+
+/**
+ * Company: Resource Management Associates
+ *
+ * @author <a href="mailto:adam@rmanet.com">Adam Korynta</a>
+ * @since 04-26-2019
+ */
+public class JasperReportRunner
+{
+
+}
