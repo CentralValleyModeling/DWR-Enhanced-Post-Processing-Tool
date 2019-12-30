28de9d0ee63d943ac3bfc1bc8875085b6b5ae361
Adam Korynta <adam@rmanet.com>
2019-12-30 15:01:54 -0800

    DE-542, DE-276 remove triangle standard deviation from box plots

--- eppt-trend-reporting/src/test/resources/dwr_eppt/config/trendreporting/js/boxandwhisker.js
+++ eppt-trend-reporting/src/test/resources/dwr_eppt/config/trendreporting/js/boxandwhisker.js
@@ -81,7 +81,7 @@ function getPlotlySeries(datum, periodMonths) {
             marker: {
                 color: datum[i]['scenario_color']
             },
-            boxmean: 'sd',
+            boxmean: true,
             boxpoints: false
         });
     }
