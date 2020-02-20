8a66c7420f4e9564037f7db772920e1134b9c5fa
Adam Korynta <adam@rmanet.com>
2020-02-20 11:58:34 -0800

    DE-656, DE-669 move all annual filters into a single table

--- eppt-nbui/src/main/config/trendreporting/js/summary.js
+++ eppt-nbui/src/main/config/trendreporting/js/summary.js
@@ -22,205 +22,224 @@ function getHeaders(data) {
     return headers;
 }
 
-function buildTable(data) {
-    let headers = getHeaders(data);
-    let values = [];
-    let colors = [];
-    for (let i = 0; i < data.length; i++) {
-        let scenarioArr = values[0];
-        if (!scenarioArr) {
-            scenarioArr = [];
-            values[0] = scenarioArr;
-        }
-        colors.push(data[i]['scenario_color']);
-        scenarioArr.push(data[i]['scenario_name']);
-        let timeSeries = data[i]['primary_data']['statistically_computed_time_series_wyt'][0];
-        let k = 0;
-        for (; k < timeSeries.length; k++) {
-            let val = timeSeries[k]['water_year_period_values'];
-            let periodValues = values[1 + k];
-            if (!periodValues) {
-                periodValues = [];
-                values[1 + k] = periodValues;
-            }
-            periodValues.push(val);
-        }
-    }
-    return {
-        type: 'table',
-        header: {
-            values: headers,
-            align: "center",
-            line: {width: 1, color: 'black'},
-            font: PLOTLY_FONT
-        },
-        cells: {
-            format: ['',',.3r%'],
-            values: values,
-            line: {color: "black", width: 1},
-            align: ["left", "center"],
-            font: {family: PLOTLY_FONT['family'], color: [colors]}
-        },
-        domain: {x: [0, 1], y: [0, 0.3]}
-    };
-}
-
 function getAcronym(text) {
     let matches = text.match(/\b(\w)/g);
     return matches.join('');
 }
 
-function buildMatrixData(data, tableValues) {
-    let header = [['Scenario'], [data['scenario_run_data'][0]['primary_data']['units'][0]]];
-    let scenarios = [];
-    let volumes = [];
-    let colors = [];
-    let headerColors = [];
-    let format = [];
-    for (let i = 0; i < data['scenario_run_data'].length; i++) {
-        let scenarioName = data['scenario_run_data'][i]['scenario_name'];
-        scenarios.push('<b>' + scenarioName + '</b>');
-        headerColors.push(data['scenario_run_data'][i]['scenario_color']);
-        volumes.push(tableValues[i]);
-        if (i < data['scenario_run_data'].length - 1) {
-            header.push(['Difference from<br>' + scenarioName]);
-        }
+function buildAnnualPeriodCells(data) {
+    let retval = ['Long Term'];
+    for (let i = 1; i < data['scenario_run_data'].length; i++) {
+        retval.push('');
     }
-    let headerFormat = [];
-    let valueFormat = [];
-    for(let i = 0; i < header.length - 1; i++){
-        headerFormat.push('');
-        valueFormat.push(FORMATTER);
-    }
-    format.push(headerFormat);
-    format.push(valueFormat);
 
-    colors.push(headerColors);
-    let values = [scenarios, volumes];
-    for (let i = 1; i < volumes.length; i++) {
-        let diffValues = [];
-        let diffFormat = [];
-        for (let j = 0; j < data['scenario_run_data'].length; j++) {
-            if (j < i) {
-                diffValues.push('');
-                diffFormat.push('');
-            } else {
-                let diff = tableValues[j] - tableValues[i - 1];
-                diffValues.push(diff);
+    if(data['scenario_run_data']){
+        let scenarioData = data['scenario_run_data'][0];
+        let scenarioPrimaryData = scenarioData['primary_data'];
+        if(scenarioPrimaryData['statistically_computed_time_series_wyt']) {
+            let wytStats = scenarioPrimaryData['statistically_computed_time_series_wyt'][0];
+            for (let j = 0; j < wytStats.length; j++) {
+                retval.push(wytStats[j]['water_year_period']);
+                for (let i = 1; i < data['scenario_run_data'].length; i++) {
+                    retval.push('');
+                }
             }
         }
-        values.push(diffValues);
-        format.push(diffFormat);
     }
-    return {
+    return retval;
+}
+
+function buildScenarioCells(data) {
+    let retval = [];
+    for (let i = 0; i < data['scenario_run_data'].length; i++) {
+        retval.push(data['scenario_run_data'][i]['scenario_name']);
+    }
+
+    if(data['scenario_run_data']){
+        let scenarioData = data['scenario_run_data'][0];
+        let scenarioPrimaryData = scenarioData['primary_data'];
+        if(scenarioPrimaryData['statistically_computed_time_series_wyt']) {
+            let wytStats = scenarioPrimaryData['statistically_computed_time_series_wyt'][0];
+            for (let j = 0; j < wytStats.length; j++) {
+                for (let i = 0; i < data['scenario_run_data'].length; i++) {
+                    retval.push(data['scenario_run_data'][i]['scenario_name']);
+                }
+            }
+        }
+    }
+    return retval;
+}
+
+function buildScenarioColors(data) {
+    let retval = [];
+    for (let i = 0; i < data['scenario_run_data'].length; i++) {
+        retval.push(data['scenario_run_data'][i]['scenario_color']);
+    }
+
+    if(data['scenario_run_data']){
+        let scenarioData = data['scenario_run_data'][0];
+        let scenarioPrimaryData = scenarioData['primary_data'];
+        if(scenarioPrimaryData['statistically_computed_time_series_wyt']) {
+            let wytStats = scenarioPrimaryData['statistically_computed_time_series_wyt'][0];
+            for (let j = 0; j < wytStats.length; j++) {
+                for (let i = 0; i < data['scenario_run_data'].length; i++) {
+                    retval.push(data['scenario_run_data'][i]['scenario_color']);
+                }
+            }
+        }
+    }
+    return retval;
+}
+
+function buildScenarioValues(data) {
+    let retval = [];
+    for (let i = 0; i < data['scenario_run_data'].length; i++) {
+        let scenarioPrimaryData = data['scenario_run_data'][i]['primary_data'];
+        let longTermValue = scenarioPrimaryData['statistically_computed_time_series_yearly'][0];
+        retval.push(longTermValue);
+    }
+
+    if(data['scenario_run_data']){
+        let scenarioData = data['scenario_run_data'][0];
+        let scenarioPrimaryData = scenarioData['primary_data'];
+        if(scenarioPrimaryData['statistically_computed_time_series_wyt']) {
+            let wytStats = scenarioPrimaryData['statistically_computed_time_series_wyt'][0];
+            for (let j = 0; j < wytStats.length; j++) {
+                for (let i = 0; i < data['scenario_run_data'].length; i++) {
+                    retval.push(data['scenario_run_data'][i]['primary_data']['statistically_computed_time_series_wyt'][0][j]['water_year_period_values']);
+                }
+            }
+        }
+    }
+    return retval;
+}
+
+function buildScenarioValuesDiff(data, diffIndex) {
+    let retval = [];
+    for (let i = 0; i < data['scenario_run_data'].length; i++) {
+        let currentValue = data['scenario_run_data'][i]['primary_data']['statistically_computed_time_series_yearly'][0];
+        let diffValue = data['scenario_run_data'][diffIndex]['primary_data']['statistically_computed_time_series_yearly'][0];
+        if(diffIndex < i){
+            retval.push(currentValue - diffValue);
+        }
+        else{
+            retval.push('');
+        }
+    }
+
+    if(data['scenario_run_data']){
+        let scenarioData = data['scenario_run_data'][0];
+        let scenarioPrimaryData = scenarioData['primary_data'];
+        if(scenarioPrimaryData['statistically_computed_time_series_wyt']) {
+            let wytStats = scenarioPrimaryData['statistically_computed_time_series_wyt'][0];
+            for (let j = 0; j < wytStats.length; j++) {
+                for (let i = 0; i < data['scenario_run_data'].length; i++) {
+                    let currentValue = data['scenario_run_data'][i]['primary_data']['statistically_computed_time_series_wyt'][0][j]['water_year_period_values'];
+                    let diffValue = data['scenario_run_data'][diffIndex]['primary_data']['statistically_computed_time_series_wyt'][0][j]['water_year_period_values'];
+                    if(diffIndex < i){
+                        retval.push(currentValue - diffValue);
+                    }
+                    else{
+                        retval.push('');
+                    }
+                }
+            }
+        }
+    }
+    return retval;
+}
+
+function buildScenarioValuesDiffFormat(data, diffIndex) {
+    let retval = [];
+    for (let i = 0; i < data['scenario_run_data'].length; i++) {
+        if(diffIndex < i){
+            retval.push(FORMATTER);
+        }
+        else{
+            retval.push('');
+        }
+    }
+
+    if(data['scenario_run_data']){
+        let scenarioData = data['scenario_run_data'][0];
+        let scenarioPrimaryData = scenarioData['primary_data'];
+        if(scenarioPrimaryData['statistically_computed_time_series_wyt']) {
+            let wytStats = scenarioPrimaryData['statistically_computed_time_series_wyt'][0];
+            for (let j = 0; j < wytStats.length; j++) {
+                for (let i = 0; i < data['scenario_run_data'].length; i++) {
+                    if(diffIndex < i){
+                        retval.push(FORMATTER);
+                    }
+                    else{
+                        retval.push('');
+                    }
+                }
+            }
+        }
+    }
+    return retval;
+}
+
+function plot(data) {
+    FORMATTER = getD3Formatter(data['scenario_run_data'][0]['primary_data']['full_time_series'][0]);
+    let header = [['<b>Period</b>'], ['<b>Scenario</b>'], ['<b>' + data['scenario_run_data'][0]['primary_data']['units'][0]] + '</b>'];
+    let annualPeriods = buildAnnualPeriodCells(data);
+    let scenarios = buildScenarioCells(data);
+    let colors = buildScenarioColors(data);
+    let format = [];
+    let periodNameFormat = [];
+    let scenarioFormat = [];
+    let periodValues = buildScenarioValues(data);
+    let periodValuesFormat = [];
+    let values = [annualPeriods, scenarios, periodValues];
+    format.push(periodNameFormat);
+    format.push(scenarioFormat);
+    format.push(periodValuesFormat);
+    for (let i = 0; i < data['scenario_run_data'].length; i++) {
+        let scenarioData = data['scenario_run_data'][i];
+        periodNameFormat.push('');
+        scenarioFormat.push('');
+        periodValuesFormat.push(FORMATTER);
+        if (i !== data['scenario_run_data'].length - 1) {
+            header.push('<b>Diff From<br>' + scenarioData['scenario_name'] + '</b>');
+            values.push(buildScenarioValuesDiff(data, i));
+        }
+        format.push(buildScenarioValuesDiffFormat(data, i));
+
+    }
+
+    let tableData = {
         type: 'table',
         header: {
             values: header,
             align: "center",
-            line: {width: 1, color: 'black'},
-            font: {family: PLOTLY_FONT['family'], size: 11}
+            font: {family: PLOTLY_FONT['family']}
         },
         cells: {
-            format: ['',',.3r%'],
+            format: format,
             values: values,
-            align: ['left', 'center'],
-            height: 8,
-            line: {color: "black", width: 1},
-            font: {family: PLOTLY_FONT['family'], color: colors, size: [9, 11]}
+            align: ['left', 'left', 'center'],
+            font: {family: PLOTLY_FONT['family'], color: ['', colors]}
         }
     };
-}
 
-function tabulateLongTermPeriod(data) {
     let layout = {
         font: PLOTLY_FONT,
-        bgcolor: 'red',
         title: {
-            text: '<b>Long Term</b>',
-            bgcolor: 'red'
-        },
-        height: tableHeight,
-        margin: {
-            l: 10,
-            r: 10,
-            b: 20,
-            t: 45,
-            pad: 4
+            text: '<b>' + data['gui_link_title'] + '<br>' + data['month_period_title'] + '<br>' + data['statistics'] + '</b>'
         }
     };
-    let longTermData = [];
-    for (let i = 0; i < data['scenario_run_data'].length; i++) {
-        longTermData[i] = data['scenario_run_data'][i]['primary_data']['statistically_computed_time_series_yearly'][0];
-    }
-    let tableData = buildMatrixData(data, longTermData);
-    Plotly.newPlot('water-year-matrix0', [tableData], layout, {
+    let elementid = "tester";
+    Plotly.newPlot(elementid, [tableData], layout, {
         displaylogo: false,
         displayModeBar: false,
         responsive: false
     });
-    let elementid = "water-year-matrix0";
     $("#" + elementid).mousedown((ev) => {
         if (ev.which === 3) {
             openContextMenu("#" + elementid, ev, () => plotlyCopyToClipboard(elementid), plotlyExportFunction(document.getElementById(elementid)));
         }
     });
-}
-
-function tabulateWaterYearPeriod(waterYearPeriod, data, tableValues, i) {
-    let layout = {
-        font: PLOTLY_FONT,
-        bgcolor: 'red',
-        title: {
-            text: '<b>' + waterYearPeriod.toUpperCase() + ' (' + getAcronym(waterYearPeriod) + ')' + '</b>',
-            bgcolor: 'red'
-        },
-        height: tableHeight,
-        margin: {
-            l: 10,
-            r: 10,
-            b: 20,
-            t: 45,
-            pad: 4
-        }
-    };
-    let tableData = buildMatrixData(data, tableValues[i]);
-    Plotly.newPlot('water-year-matrix' + i, [tableData], layout, {
-        displaylogo: false,
-        displayModeBar: false,
-        responsive: false
-    });
-    let elementid = "water-year-matrix" + i;
-    $("#" + elementid).mousedown((ev) => {
-        if (ev.which === 3) {
-            openContextMenu("#" + elementid, ev, () => plotlyCopyToClipboard(elementid), plotlyExportFunction(document.getElementById(elementid)));
-        }
-    });
-}
-
-var tableHeight = 300;
-
-function plot(data) {
-    let title = document.getElementById('title');
-    while (title.firstChild) {
-        title.removeChild(title.firstChild);
-    }
-    let element = document.createElement('h2');
-    title.appendChild(element);
-    element.appendChild(document.createTextNode(data['gui_link_title']));
-    element.appendChild(document.createElement('br'));
-    element.appendChild(document.createTextNode(data['month_period_title']));
-    element.appendChild(document.createElement('br'));
-    element.appendChild(document.createTextNode(data['statistics']));
-    tableHeight = 100 + data['scenario_run_data'].length * 35;
-    let tableValues = buildTable(data['scenario_run_data'])['cells']['values'];
-    tabulateLongTermPeriod(data);
-    for (let i = 1; i < tableValues.length; i++) {
-        let arr = data['scenario_run_data'][0]['primary_data']['statistically_computed_time_series_wyt'][0][i - 1];
-        if (arr) {
-            let waterYearPeriod = arr['water_year_period'];
-            tabulateWaterYearPeriod(waterYearPeriod, data, tableValues, i);
-        }
-    }
 }
 
 function plotlyCopyToClipboard(element) {
@@ -228,9 +247,6 @@ function plotlyCopyToClipboard(element) {
     let layout = plot.layout;
     let data1 = plot.data;
     var text = layout['title']['text'];
-    if (layout['yaxis'] && layout['yaxis']['title']) {
-        text += '\nScenario\t' + layout['yaxis']['title']['text'];
-    }
     text += '\n';
     let datum = data1[data1.length - 1];
     for (let i = 0; i < datum['header']['values'].length; i++) {
